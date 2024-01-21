a23e523d21e427b056328663e17a827f6b0e6cab
Maguette Diouf <dioufmaguette17@gmail.com>
2024-01-21 21:45:27 +0100

    commit theme enfant

--- wordpress/wp-content/themes/jobscout_enfant/functions.php
+++ wordpress/wp-content/themes/jobscout_enfant/functions.php
@@ -1,88 +1,31 @@
 <?php
 /**
- * JobScout functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package JobScout
- */
-
-$jobscout_theme_data = wp_get_theme();
-if( ! defined( 'JOBSCOUT_THEME_VERSION' ) ) define ( 'JOBSCOUT_THEME_VERSION', $jobscout_theme_data->get( 'Version' ) );
-if( ! defined( 'JOBSCOUT_THEME_NAME' ) ) define( 'JOBSCOUT_THEME_NAME', $jobscout_theme_data->get( 'Name' ) );
-
-/**
- * Implement Local Font Method functions.
- */
-require get_template_directory() . '/inc/class-webfont-loader.php';
-
-/**
- * Custom Functions.
- */
-require get_template_directory() . '/inc/custom-functions.php';
-
-/**
- * Standalone Functions.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Template Functions.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Custom functions for selective refresh.
- */
-require get_template_directory() . '/inc/partials.php';
-
-if( jobscout_is_rara_theme_companion_activated() ) :
-	/**
-	 * Modify filter hooks of RTC plugin.
-	 */
-	require get_template_directory() . '/inc/rtc-filters.php';
-endif;
-
-/**
- * Custom Controls
- */
-require get_template_directory() . '/inc/custom-controls/custom-control.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer/customizer.php';
-
-/**
- * Widgets
- */
-require get_template_directory() . '/inc/widgets.php';
-
-/**
- * Metabox
- */
-require get_template_directory() . '/inc/metabox.php';
-
-/**
- * Getting Started
-*/
-require get_template_directory() . '/inc/getting-started/getting-started.php';
-
-/**
- * Plugin Recommendation
-*/
-require get_template_directory() . '/inc/tgmpa/recommended-plugins.php';
-
-/**
- * Add theme compatibility function for woocommerce if active
-*/
-if( jobscout_is_woocommerce_activated() ){
-    require get_template_directory() . '/inc/woocommerce-functions.php';    
+** activation theme
+**/
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+ wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
 }
 
-/**
- * Modify filter hooks of WP Job Manager plugin.
- */
-if( jobscout_is_wp_job_manager_activated() ) :
-	require get_template_directory() . '/inc/wp-job-manager-filters.php';
-endif;
+
+
+
+
+
+function custom_enqueue_scripts() { 
+//Ajout des CSS style_sydEnfant
+        wp_enqueue_style( 'custom', get_stylesheet_directory_uri() . '/style_sydEnfant.css', array(), NULL, NULL);
+    
+    //Ajout des JavaScript (appelés dans le footer)
+        wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/scripts_sydEnfant.js', array( 'jquery' ),  NULL, true);
+        
+   
+}
+add_action('wp_enqueue_scripts', 'custom_enqueue_scripts', 101);
+
+
+
+
+
+?>
